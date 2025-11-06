acb88a0923527ecdb801081001b434212a236e18
Kamilo <camilomontenegr0sierra@gmail.com>
2025-11-06 15:22:06 +0100

    update: objects and main

--- Acceso_datos/Coding/Bonana_Project_Este_Si/FruitShop/src/Fruit.java
+++ Acceso_datos/Coding/Bonana_Project_Este_Si/FruitShop/src/Fruit.java
@@ -1,3 +1,62 @@
 public class Fruit {
 
+    private int id;
+    private String name;
+    private double pricePerKg;
+    private int stockKg;
+
+    public Fruit() {}
+
+    public Fruit(int id, String name, double pricePerKg, int stockKg) {
+        this.id = id;
+        this.name = name;
+        this.pricePerKg = pricePerKg;
+        this.stockKg = stockKg;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPricePerKg() {
+        return pricePerKg;
+    }
+
+    public void setPricePerKg(double pricePerKg) {
+        this.pricePerKg = pricePerKg;
+    }
+
+    public int getStockKg() {
+        return stockKg;
+    }
+
+    public void setStockKg(int stockKg) {
+        this.stockKg = stockKg;
+    }
+
+    @Override
+    public String toString() {
+        return "Fruit{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", pricePerKg=" + pricePerKg +
+                ", stockKg=" + stockKg +
+                '}';
+    }
+
+    public String toFileFormat() {
+        return id + ";" + name + ";" + pricePerKg + ";" + stockKg;
+    }
 }
--- Acceso_datos/Coding/Bonana_Project_Este_Si/FruitShop/src/FruitShopApp.java
+++ Acceso_datos/Coding/Bonana_Project_Este_Si/FruitShop/src/FruitShopApp.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class FruitShopApp {
+
+    private static ArrayList<Fruit> inventory = new ArrayList<>();
+
+    private static Scanner sc = new Scanner(System.in);
+
+    private static final String FILE_PATH = "data/fruits.txt";
+
+    //contador para generar IDs únicos
+    private static int nextId = 1;
+
+
+    public static void main(String[] args) {
+        System.out.println("╔════════════════════════════════════╗");
+        System.out.println("║         Frutería Mari-CarMen       ║");
+        System.out.println("╚════════════════════════════════════╝");
+        System.out.println();
+    }
 }
