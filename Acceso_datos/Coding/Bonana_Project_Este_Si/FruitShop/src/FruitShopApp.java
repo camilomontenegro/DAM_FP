3d6ddc48fb637ba40ac8dd83318ab8d38fd880e5
Kamilo <camilomontenegr0sierra@gmail.com>
2025-11-06 17:09:43 +0100

    Update to bonana

--- Acceso_datos/Coding/Bonana_Project_Este_Si/FruitShop/src/FruitShopApp.java
+++ Acceso_datos/Coding/Bonana_Project_Este_Si/FruitShop/src/FruitShopApp.java
@@ -18,5 +18,47 @@ public class FruitShopApp {
         System.out.println("║         Frutería Mari-CarMen       ║");
         System.out.println("╚════════════════════════════════════╝");
         System.out.println();
+
+        //createDataFolder();
+        boolean exit = false;
+
+        while (!exit) {
+            showMenu();
+            int userChoice = readUserChoice();
+            System.out.println();
+
+            switch (userChoice) {
+                case 1 -> addFruit();
+                /*case 2 -> listFruits();
+                case 3 -> exportToFile();
+                case 4 -> importFromFile();*/
+                case 5 -> {
+                    exit = true;
+                    System.out.println("Hasta luego Mari Loli!");
+                }
+                default -> System.out.println("Opción no válida, vuelve a elegir");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+
+    private static void showMenu() {
+        System.out.println("════════════════════════════════════");
+        System.out.println("MENU: \n1. Añadir fruta al inventario \n2. Listar frutas en inventario \n3. Exportar a TXT \n4. Importar desde TXT \n5. salir");
+        System.out.println("════════════════════════════════════");
+        System.out.print("Selecciona opción: ");
+    }
+
+    private static int readUserChoice() {
+        try {
+            return Integer.parseInt(sc.nextLine().trim());
+        } catch (NumberFormatException e) {
+            return -1;
+        }
+    }
+
+    private static String addFruit() {
+        return "";
     }
 }
